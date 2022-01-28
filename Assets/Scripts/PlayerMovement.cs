89abb7902d5f8f84835147637a2a6bf011212539
Sampakk <90028990+Sampakk@users.noreply.github.com>
2022-01-28 21:22:22 +0200

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed = 4f;
+    public float jumpHeight = 3f;
+    float moveX;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundDistance = 0.45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if (isGrounded())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        }
+        Debug.Log(isGrounded());    
+    }   
+    private void FixedUpdate()
+    {
+        Vector2 moveDir = transform.right * moveX + transform.forward;
+        moveDir = Vector2.ClampMagnitude(moveDir, 1f);
+
+        //Add force
+        rb.AddForce(moveDir, ForceMode2D.Impulse);
+
+        //Clamp movement
+        float verticalVelocity = Mathf.Clamp(rb.velocity.y, float.MinValue, jumpHeight);
+        Vector2 horizontalVelocity = new Vector2(moveDir.x * moveSpeed, verticalVelocity);
+        rb.velocity = horizontalVelocity;
+    }
+    bool isGrounded()
+    {
+        if (Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundLayer)) return true;
+        return false;
+    }
+}   
