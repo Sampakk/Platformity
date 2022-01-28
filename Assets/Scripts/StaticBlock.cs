a8826bd9ecbb29bbb86a068c407ae31b81129c5c
Niko-Viitio <dnivi009@edu.xamk.fi>
2022-01-28 23:32:05 +0200

    Fixed static block

--- Assets/Scripts/StaticBlock.cs
+++ Assets/Scripts/StaticBlock.cs
@@ -5,33 +5,48 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class StaticBlock : MonoBehaviour
 {
+    Vector3 lastPos, lastScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Application.isPlaying)
+            Destroy(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Snap scale
-        Vector3 scale = transform.localScale;
-        scale.x = Mathf.RoundToInt(scale.x);
-        scale.y = Mathf.RoundToInt(scale.y);
-        scale.z = 1;
+        if (lastPos == Vector3.zero || lastScale == Vector3.zero)
+            return;
 
-        transform.localScale = scale;
+        if (transform.localScale != lastScale || transform.position != lastPos)
+        {
+            //Snap scale
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.RoundToInt(scale.x);
+            scale.y = Mathf.RoundToInt(scale.y);
+            scale.z = 1;
 
-        //Check offset for position
-        float xOffset = (scale.x % 2 == 0) ? 1f : 0.5f; 
-        float yOffset = (scale.y % 2 == 0) ? 1f : 0.5f; 
+            transform.localScale = scale;
 
-        //Snap position
-        Vector3 position = transform.position;
-        position.x = Mathf.RoundToInt(position.x) + xOffset;
-        position.y = Mathf.RoundToInt(position.y) - yOffset;
-        position.z = 0;
+            //Check offset for position
+            float xOffset = (scale.x % 2 == 0) ? 1f : 0.5f;
+            float yOffset = (scale.y % 2 == 0) ? 1f : 0.5f;
 
-        transform.position = position;
+            //Snap position
+            Vector3 position = transform.position;
+            position.x = Mathf.RoundToInt(position.x) + xOffset;
+            position.y = Mathf.RoundToInt(position.y) - yOffset;
+            position.z = 0;
+
+            transform.position = position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        lastPos = transform.position;
+        lastScale = transform.localScale;
     }
 }
