870f4438d511dce2e16584cb7bb41ad485fd0e21
OtslaOtt <ott.otsla@gmail.com>
2022-01-29 02:10:27 +0200

    MovingPlatform script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public float maxX;
+    public float minX;
+    private float platformPosition;
+    private int direction = 1;
+    public float speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        platformPosition = rb.transform.position.x;
+
+        if (platformPosition >= maxX)
+            direction = 0;
+        if (platformPosition <= minX)
+            direction = 1;
+
+
+        if (direction == 1)
+            MoveRight();
+        else
+            MoveLeft();
+        
+    }
+
+    void MoveRight()
+    {
+        rb.velocity = Vector2.right * speed;
+    }
+
+    void MoveLeft()
+    {
+        rb.velocity = Vector2.left * speed;
+    }
+}
