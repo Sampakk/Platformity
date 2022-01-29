b45ed5ca23b8f04192507dd67b2d92e61af4b8f7
Niko-Viitio <dnivi009@edu.xamk.fi>
2022-01-30 00:56:46 +0200

    Mainmenu better header

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public RectTransform header;
     public GameObject levelsRoot;
 
+    TextMeshProUGUI headerText;
+    float headerFontSize;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Get header text
+        headerText = header.GetComponent<TextMeshProUGUI>();
+        headerFontSize = headerText.fontSize;
+
+        //Setup level buttons
         Button[] levelButtons = levelsRoot.GetComponentsInChildren<Button>();
 
         for (int i = 0; i < levelButtons.Length; i++)
@@ -28,7 +37,7 @@ public class MainMenu : MonoBehaviour
                 string levelPrefsName = "Level" + levelIndex;
                 if (PlayerPrefs.GetInt(levelPrefsName) == 0)
                     button.interactable = false;
-            }          
+            }
         }
     }
 
@@ -40,6 +49,9 @@ public class MainMenu : MonoBehaviour
         eulers.z = Mathf.Sin(Time.time * 2f) * 5f;
 
         header.rotation = Quaternion.Euler(eulers);
+
+        //Scale font
+        headerText.fontSize = headerFontSize + Mathf.Sin(Time.time * 4f) * 5f;
     }
 
     public void LoadLevel(int index)
