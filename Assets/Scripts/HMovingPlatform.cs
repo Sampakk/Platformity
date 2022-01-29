7a59729294ccb1be50532bc323383adb576962fd
OtslaOtt <ott.otsla@gmail.com>
2022-01-29 02:27:30 +0200

    Differentiated Vertical and Horizontal moving platform scripts

--- Assets/Scripts/HMovingPlatform.cs
+++ Assets/Scripts/HMovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class HMovingPlatform : MonoBehaviour
 {
 
     public float maxX;
