779881cac4271c62d223eec823e54f14ec1cab37
Sampakk <90028990+Sampakk@users.noreply.github.com>
2022-01-30 13:54:11 +0200

    falling spike maps

--- Assets/Scripts/SpikeSpawner.cs
+++ Assets/Scripts/SpikeSpawner.cs
@@ -6,21 +6,21 @@ public class SpikeSpawner : MonoBehaviour
 {
     public float spawnInterval = 1;
     public GameObject fallingSpike;
-
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-
+        timer = spawnInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnInterval -= Time.deltaTime;
-        if (spawnInterval <= 0)
+        timer -= Time.deltaTime;
+        if (timer <= 0)
         {
             Instantiate(fallingSpike, new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y - 1.5f, gameObject.transform.localPosition.z), Quaternion.identity);
-            spawnInterval = 2;
+            timer = spawnInterval;
 
         }
     }
