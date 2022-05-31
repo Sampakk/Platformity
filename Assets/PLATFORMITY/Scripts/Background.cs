0a310f3ea91ecb25b098ff1f082b10655c7f9054
Sampakk <90028990+Sampakk@users.noreply.github.com>
2022-05-31 12:05:16 +0300

    Scale for bg

--- Assets/PLATFORMITY/Scripts/Background.cs
+++ Assets/PLATFORMITY/Scripts/Background.cs
@@ -11,15 +11,21 @@ public class Background : ScriptableObject
     public Sprite middleSprite;
     public Sprite foreSprite;
 
+    [Header("Back values")]
     public float parallaxAmountXb;
     public float parallaxAmountYb;
     public float parallaxAmountZrotb;
+    public float parallaxAmountScaleb;
 
+    [Header("Middle values")]
     public float parallaxAmountXm;
     public float parallaxAmountYm;
     public float parallaxAmountZrotm;
+    public float parallaxAmountScalem;
 
+    [Header("Fore values")]
     public float parallaxAmountXf;
     public float parallaxAmountYf;
     public float parallaxAmountZrotf;
+    public float parallaxAmountScalef;
 }
