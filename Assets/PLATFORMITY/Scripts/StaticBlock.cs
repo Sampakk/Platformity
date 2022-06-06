a8f3b0a0ea8f1d55541d8f44b9094bd86ec99f15
Niko-Viitio <91052420+Niko-Viitio@users.noreply.github.com>
2022-06-06 12:51:44 +0300

    fixed static block running away in editor

--- Assets/PLATFORMITY/Scripts/StaticBlock.cs
+++ Assets/PLATFORMITY/Scripts/StaticBlock.cs
@@ -46,17 +46,35 @@ public class StaticBlock : MonoBehaviour
             sprite.drawMode = SpriteDrawMode.Tiled;
         }            
 
-        if (repeatTexture)
+        if (repeatTexture) //Texture repeating
         {
-            //Force scale
-            transform.localScale = Vector3.one;
+            if (lastPos == Vector3.zero || lastScale == Vector3.zero)
+                return;
 
-            //Update sprite repeating
-            sprite.size = new Vector2(width, height);
+            if (transform.position != lastPos || width != lastWidth || height != lastHeight)
+            {
+                //Force scale
+                transform.localScale = Vector3.one;
 
-            //Update colliders size
-            foreach (BoxCollider2D collider in colliders)
-                collider.size = new Vector2(width, height);
+                //Update sprite repeating
+                sprite.size = new Vector2(width, height);
+
+                //Update colliders size
+                foreach (BoxCollider2D collider in colliders)
+                    collider.size = new Vector2(width, height);
+
+                //Check offset for position
+                float xOffset = (width % 2 == 0) ? 1f : 0.5f;
+                float yOffset = (height % 2 == 0) ? 1f : 0.5f;
+
+                //Snap position
+                Vector3 position = transform.position;
+                position.x = Mathf.RoundToInt(position.x) + xOffset;
+                position.y = Mathf.RoundToInt(position.y) - yOffset;
+                position.z = 0;
+
+                transform.position = position;
+            }
         }
         else //No repeat, affect scale
         {
@@ -72,20 +90,20 @@ public class StaticBlock : MonoBehaviour
                 scale.z = 1;
 
                 transform.localScale = scale;
-            }
+
+                //Check offset for position
+                float xOffset = (scale.x % 2 == 0) ? 1f : 0.5f;
+                float yOffset = (scale.y % 2 == 0) ? 1f : 0.5f;
+
+                //Snap position
+                Vector3 position = transform.position;
+                position.x = Mathf.RoundToInt(position.x) + xOffset;
+                position.y = Mathf.RoundToInt(position.y) - yOffset;
+                position.z = 0;
+
+                transform.position = position;
+            }       
         }
-
-        //Check offset for position
-        float xOffset = (width % 2 == 0) ? 1f : 0.5f;
-        float yOffset = (height % 2 == 0) ? 1f : 0.5f;
-
-        //Snap position
-        Vector3 position = transform.position;
-        position.x = Mathf.RoundToInt(position.x) + xOffset;
-        position.y = Mathf.RoundToInt(position.y) - yOffset;
-        position.z = 0;
-
-        transform.position = position;
     }
 
     void LateUpdate()
